f97de84d21c13ecb45eb615dca95c6e8190c9970
himoza <impocanal@gmail.com>
2025-11-07 14:42:37 +0500

    make wrapper for model U-Net

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,3 +1,5 @@
+import math
+
 from torch import nn
 import torch.nn.functional as F
 
@@ -62,3 +64,39 @@ class UNet(nn.Module):
             x = up(x, blocks[-i - 1])
         
         return self.last(x)
+
+
+class UNetWrapper(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        
+        self.input_batchnorm = nn.BatchNorm2d(kwargs['in_channels'])
+        self.unet = UNet(**kwargs)
+        self.final = nn.Sigmoid()
+        
+        self.__init_weights()
+    
+    def _init_weights(self):
+        init_set = {
+            nn.Conv2d,
+            nn.Conv3d,
+            nn.ConvTranspose2d,
+            nn.ConvTranspose3d,
+            nn.Linear,
+        }
+        for m in self.modules():
+            if type(m) in init_set:
+                nn.init.kaiming_normal_(
+                    m.weight.data, a=0, mode='fan_out', nonlinearity='relu',
+                )
+                if m.bias is not None:
+                    fan_in, fan_out = \
+                        nn.init._calculate_fan_in_and_fan_out(m.weight.data)
+                    bound = 1 / math.sqrt(fan_out)
+                    nn.init.normal_(m.bias, -bound, bound)
+    
+    def forward(self, input_batch):
+        bn_output = self.input_batchnorm(input_batch)
+        un_output = self.unet(bn_output)
+        fn_output = self.final(un_output)
+        return fn_output
